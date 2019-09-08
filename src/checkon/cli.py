839aaf472619a45757be6faf4de78bd5d19e2882
metatooling <noreply+zztokdufhjcfspskvqvt@cordaz.com>
2019-09-08 23:15:11 +0000

    Support urls file

--- src/checkon/cli.py
+++ src/checkon/cli.py
@@ -5,11 +5,20 @@ import click
 from . import app
 
 
+def run_cli(inject, project_url, project_urls_file):
+    if project_urls_file:
+        project_urls = (
+            list(project_url) + pathlib.Path(project_urls_file).read_text().splitlines()
+        )
+    app.run_many(inject=inject, project_urls=project_urls)
+
+
 cli = click.Command(
     "run",
     params=[
         click.Argument(["inject"], type=lambda x: pathlib.Path(x).resolve()),
-        click.Argument(["project_url"]),
+        click.Argument(["project_url"], nargs=-1, required=False),
+        click.Option(["--project-urls-file"], type=click.File()),
     ],
-    callback=lambda **kw: print(app.run(**kw)),
+    callback=run_cli,
 )
